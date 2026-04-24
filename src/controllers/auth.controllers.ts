8e4d38874fb989f2aa8e7dd99b4ba1d8b06b38a2
arunkhairwar2 <arun.khairwar@leadows.com>
2026-04-24 18:19:52 +0530

    Feat: implement user authentication models, registration schema, and controller logic

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,12 +1,16 @@
 import type { Request, Response } from "express";
+import type { RegisterSchemaType } from "../schemas/auth.schema.ts";
+import { RegisterSchema } from "../schemas/auth.schema.ts";
 
 class AuthControllers {
   register = async (req: Request, res: Response) => {
     try {
-      const { name, email, password } = req.body;
+      const registerData = req.body as RegisterSchemaType;
 
-      if (!name || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
+      const validatedUser = RegisterSchema.parse(registerData);
+
+      if (!validatedUser) {
+        return res.status(400).json({ message: "Invalid user data" });
       }
 
       // const user = await authServices.registerUser({ name, email, password });
---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,57 @@
+import z from "zod";
+
+export const phoneValidation = z
+  .string()
+  .trim()
+  .min(1, "Phone number is required")
+  .refine((val) => /^\+?[1-9]\d{6,14}$/.test(val), {
+    message: "Please enter a valid phone number.",
+  });
+
+export const emailValidation = z
+  .string()
+  .min(1, "Email is required")
+  .max(50, "Email must be less than 50 characters")
+  .trim()
+  .email("Please enter a valid email address")
+  .transform((val) => val.toLowerCase());
+
+export const passwordValidation = z
+  .string()
+  .min(1, "Password is required")
+  .min(8, "Password must be at least 8 characters")
+  .max(16, "Password must be less than 16 characters")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[0-9]/, "Password must contain at least one number")
+  .regex(
+    /[^A-Za-z0-9]/,
+    "Password must contain at least one special character",
+  );
+
+export const RegisterSchema = z.object({
+  firstName: z
+    .string()
+    .min(3, "First name must be at least 3 characters")
+    .max(100, "First name must be less than 50 characters")
+    .trim()
+    .regex(/^[A-Za-z]+$/, "First name must contain only letters"),
+  middleName: z.string().optional(),
+  lastName: z
+    .string()
+    .min(3, "Last name must be at least 3 characters")
+    .max(100, "Last name must be less than 100 characters")
+    .trim()
+    .regex(/^[A-Za-z]+$/, "Last name must contain only letters"),
+  primaryPhoneNumber: phoneValidation,
+  dob: z.date(),
+  gender: z.enum(["MALE", "FEMALE", "OTHER"]),
+  email: emailValidation,
+  password: passwordValidation,
+});
+
+export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
+// export type LoginSchemaType = z.infer<typeof LoginSchema>;
+// export type PhoneValidationType = z.infer<typeof phoneValidation>;
+// export type EmailValidationType = z.infer<typeof emailValidation>;
+// export type PasswordValidationType = z.infer<typeof passwordValidation>;
